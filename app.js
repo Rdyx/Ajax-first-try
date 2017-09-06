5b2c3379e166c6b191633899a01274518032bba7
Rdyx <tiranico.pro@gmail.com>
2017-09-06 10:18:39 +0200

    add meteo + css

--- app.js
+++ app.js
@@ -37,15 +37,18 @@
 //                 $('#test').append('OUPS');
 //             }
 //         });
-// });
-
+//});
 
 $.ajax({
-    url : 'http://api.openweathermap.org/data/2.5/forecast?id=3028641&APPID=6785cd546578eff50319fa79ce8aaa05&units=metric',
-    success: function(data){
+    url: 'http://api.openweathermap.org/data/2.5/forecast?id=3028641&APPID=6785cd546578eff50319fa79ce8aaa05&units=metric',
+    success: function (data) {
         console.log(data);
-        $('#test').html("jour 1 : Ville : " + data.city.name + '<br>Pays :  ' + data.city.country + '<br>Id Ville :  ' + data.city.id + '<br>Température ' + data.list[0].main.temp + '<br>Date :  ' + data.list[0].dt_txt+'<br>');
-        $('#test').append("jour 2 : Ville : " + data.city.name + '<br>Pays :  ' + data.city.country + '<br>Id Ville :  ' + data.city.id + '<br>Température ' + data.list[5].main.temp + '<br>Date :  ' + data.list[5].dt_txt+'<br>');
-        $('#test').append("jour 3 : Ville : " + data.city.name + '<br>Pays :  ' + data.city.country + '<br>Id Ville :  ' + data.city.id + '<br>Température ' + data.list[10].main.temp + '<br>Date :  ' + data.list[15].dt_txt+'<br>');
+        for (var i = 1; i < 8; ++i) {
+            $('#icone').append('<td class="bord fond'+i+'"><img alt="'+data.list[i].weather[0].description+'" src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png"></td>');
+            $('#temp').append('<td class="bord fond'+i+'">'+ Math.floor(data.list[i].main.temp) + '°C</td>');
+            $('#date').append('<td class="bord fond'+i+'">'+ data.list[i].dt_txt + '</td>');
+            $('#ville').append('<td class="bord fond'+i+'">'+ data.city.name + '</td>');
+            //<tr><td>" + data.city.name + '</td><td>' + data.city.country + '</td><td>' +  + '</td><td>' + Math.floor(data.list[i].main.temp) + '°C</td><td></td></tr>');
+        }
     }
 })
