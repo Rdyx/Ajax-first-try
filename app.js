da5c2b0849f61e87e3d94ce19e14ce55f6b938c9
Rdyx <tiranico.pro@gmail.com>
2017-09-08 12:29:57 +0200

    add search modul

--- app.js
+++ app.js
@@ -63,11 +63,44 @@ $.ajax({
                 windDirection = "Nord-Ouest";
             }
             $('#icone').append('<td class="bord"><img alt="'+data.list[i].weather[0].description+'" src="http://openweathermap.org/img/w/' + data.list[i].weather[0].icon + '.png"></td>');
-            $('#temp').append('<td class="bord">'+ Math.floor(data.list[i].main.temp) + '°C</td>');
+            $('#temp').append('<td class="bord"><strong>'+ Math.floor(data.list[i].main.temp) + '°C</strong></td>');
             $('#date').append('<td class="bord">'+ data.list[i].dt_txt + '</td>');
-            $('#ville').append('<td class="bord">'+ data.city.name + '</td>');
+            $('#ville').append('<td class="bord"><strong>'+ data.city.name + '</strong></td>');
             $('#wind').append('<td class="bord">Vent : '+ Math.floor(data.list[i].wind.speed * 3.6) + ' km/h <br>Direction :  ' + windDirection + '</td>')
             //<tr><td>" + data.city.name + '</td><td>' + data.city.country + '</td><td>' +  + '</td><td>' + Math.floor(data.list[i].main.temp) + '°C</td><td></td></tr>');
         }
     }
+})
+
+$('#searchGo').click(function (){
+    var villeSearch = $('#search').val();
+    $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?q='+villeSearch+'&APPID=6785cd546578eff50319fa79ce8aaa05&units=metric',
+    success: function (data) {
+        console.log(data);
+        var windDirection;
+            if(data.wind.deg < 22.5 && data.wind.deg >= 337.5){
+                windDirection = "Nord";
+            } if(data.wind.deg < 67.5 && data.wind.deg >= 22.5){
+                windDirection = "Nord-Est";
+            } if(data.wind.deg < 112.5 && data.wind.deg >= 67.5){
+                windDirection = "Est";
+            } if(data.wind.deg < 157.5 && data.wind.deg >= 112.5){
+                windDirection = "Sud-Est";
+            } if(data.wind.deg < 202.5 && data.wind.deg >= 157.5){
+                windDirection = "Sud";
+            } if(data.wind.deg < 247.5 && data.wind.deg >= 202.5){
+                windDirection = "Sud-Ouest";
+            } if(data.wind.deg < 292.5 && data.wind.deg >= 247.5){
+                windDirection = "Ouest";
+            } if(data.wind.deg < 337.5 && data.wind.deg >= 292.5){
+                windDirection = "Nord-Ouest";
+            }
+            $('#icone2').html('<td class="bord"><img alt="'+data.weather[0].description+'" src="http://openweathermap.org/img/w/' + data.weather[0].icon + '.png"></td>');
+            $('#temp2').html('<td class="bord"><strong>'+ Math.floor(data.main.temp) + '°C</strong></td>');
+            $('#ville2').html('<td class="bord"><strong>'+ data.name + '</strong></td>');
+            $('#wind2').html('<td class="bord">Vent : '+ Math.floor(data.wind.speed * 3.6) + ' km/h <br>Direction :  ' + windDirection + '</td>')
+            //<tr><td>" + data.city.name + '</td><td>' + data.city.country + '</td><td>' +  + '</td><td>' + Math.floor(data.list[i].main.temp) + '°C</td><td></td></tr>');
+        }
+    })
 })
